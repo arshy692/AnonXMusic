c2ad1ce206e4d1969dd1d2ab24db30cd4837e2ef
arshy692 <162564228+arshy692@users.noreply.github.com>
2024-06-14 14:30:44 +0530

    Update bot.py
    Signed-off-by: arshy692 <162564228+arshy692@users.noreply.github.com>

--- AnonXMusic/core/bot.py
+++ AnonXMusic/core/bot.py
@@ -31,24 +31,7 @@ class Anony(Client):
                 chat_id=config.LOGGER_ID,
                 text=f"<u><b>» {self.mention} ʙᴏᴛ sᴛᴀʀᴛᴇᴅ :</b><u>\n\nɪᴅ : <code>{self.id}</code>\nɴᴀᴍᴇ : {self.name}\nᴜsᴇʀɴᴀᴍᴇ : @{self.username}",
             )
-        except (errors.ChannelInvalid, errors.PeerIdInvalid):
-            LOGGER(__name__).error(
-                "Bot has failed to access the log group/channel. Make sure that you have added your bot to your log group/channel."
-            )
-            
-        except Exception as ex:
-            LOGGER(__name__).error(
-                f"Bot has failed to access the log group/channel.\n  Reason : {type(ex).__name__}."
-            )
-            
-
-        a = await self.get_chat_member(config.LOGGER_ID, self.id)
-        if a.status != ChatMemberStatus.ADMINISTRATOR:
-            LOGGER(__name__).error(
-                "Please promote your bot as an admin in your log group/channel."
-            )
-            
-        LOGGER(__name__).info(f"Music Bot Started as {self.name}")
+ LOGGER(__name__).info(f"Music Bot Started as {self.name}")
 
     async def stop(self):
         await super().stop()
